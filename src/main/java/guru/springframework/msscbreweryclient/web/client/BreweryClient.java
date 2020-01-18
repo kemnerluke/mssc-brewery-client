e392dbb0490ed1e082497a3155bf2904a54f2486
Luke <kemnerluke@gmail.com>
2020-01-17 22:13:55 -0500

    added post functionality

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 
@@ -24,6 +25,10 @@ public class BreweryClient {
 
     public BeerDTO getBeerById(UUID uuid){
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDTO.class);
+    }
+
+    public URI saveNewBeer(BeerDTO beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
     }
 
     public void setApihost(String apihost) {
